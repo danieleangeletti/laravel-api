386414f9fd21f4c2cb544a005093cf92582bdb05
danieleangeletti <angelettidaniele@gmail.com>
2024-03-15 21:05:14 +0100

    invio email tramite Mailtrap

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -11,11 +11,19 @@ use App\Models\Contact;
 // Form requests
 use App\Http\Requests\Contact\StoreRequest as ContactStoreRequest;
 
+// Helpers
+use Illuminate\Support\Facades\Mail;
+
+// Mailable
+use App\Mail\NewContact;
+
 class ContactController extends Controller
 {
     public function store(ContactStoreRequest $request)
     {
         $contact = Contact::create($request->validated());
+
+        Mail::to('danieleadmin@gmail.com')->send(new NewContact);
 
         return response()->json([
             'success' => true,
---
+++ resources/views/emails/new-contact.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New contact</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <h1>
+            Hi Daniel!
+        </h1>
+        
+        <p>
+            You received a new message from the frontend in Vue
+        </p>
+
+        <ul>
+            <li>
+                Nome: Ciccio
+            </li>
+            <li>
+                Email: cicciopasticcio@gmail.com
+            </li>
+            <li>
+                Messaggio:
+                <p>
+                    Lorem ipsum dolor sit amet
+                </p>
+            </li>
+        </ul>
+    </main>
+</body>
+</html>
